bf0f650434e2222839ba6b16591af705c97a3e2b
Tyler Dodge <tyler@tdodge.consulting>
2023-04-24 12:20:48 -0700

    Use package-install-file instead of manually adding dependencies

--- nix/package-lint.sh
+++ nix/package-lint.sh
@@ -9,25 +9,22 @@ function buildPhase() {
     export HOME=home/
     ${emacs}/bin/emacs -q --version
     wget -O melpazoid.el https://raw.githubusercontent.com/riscy/melpazoid/master/melpazoid/melpazoid.el
+    ln -s "$org_assistant" org-assistant.el
     ${emacs}/bin/emacs -q -batch \
             --eval "(require 'package)" \
             --eval "(add-to-list 'package-archives '(\"melpa\" . \"https://melpa.org/packages/\") t)" \
             --eval "(package-initialize)" \
             --eval "(package-refresh-contents)" \
-            --eval "(package-install 'deferred)" \
-            --eval "(package-install 'uuid)" \
-            --eval "(package-install 'pkg-info)" \
-            --eval "(package-install 's)" \
-            --eval "(package-install 'dash)" \
+            --eval "(package-install-file \"org-assistant.el\")" \
+            --eval "(package-install-file \"melpazoid.el\")" \
             -l package-lint \
-            -l melpazoid.el \
-            --file "$org_assistant" \
+            --file "org-assistant.el" \
             --eval "(message \"[NOT RESPONSIBLE FOR WARNINGS PRIOR TO THIS POINT]\")" \
-            --eval "(set-visited-file-name \"org-assistant.el\")" \
             --eval "(checkdoc-eval-current-buffer)" \
             --eval "(with-current-buffer \"*Style Warnings*\" (message \"%s\" (buffer-string)))" \
             --eval "(package-lint-current-buffer)" \
             --eval "(byte-compile-file \"$org_assistant\")" \
+            --eval "(require 'melpazoid)" \
             --eval "(melpazoid-check-experimentals)" \
             --eval "(with-current-buffer \"*Package-Lint*\" (message \"%s\" (buffer-string)))" | tee $out
     STATUS="${PIPESTATUS[0]}"
