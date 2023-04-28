ca9d94af967259a61d39bb6180da89cf89de9378
Tyler Dodge <tyler@tdodge.consulting>
2023-04-28 15:09:16 -0700

    Switch shell.nix version to mach nix/emacs-28.1.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/eabc38219184cc3e04a974fe31857d8e0eac098d.tar.gz") {} }:
+{ pkgs ? import (fetchGit {
+    name = "emacs-revision-28.1";
+    url = "https://github.com/NixOS/nixpkgs/";                       
+    ref = "refs/heads/nixpkgs-unstable";                     
+    rev = "b1abaab2d14493c20740de34bad772f17d1e731b";    
+  }) {} }:
 
 pkgs.mkShell {
   packages = [
