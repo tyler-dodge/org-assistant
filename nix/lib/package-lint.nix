19b7f06984f20d4955d29bc68ff053976d26985b
Tyler Dodge <tyler@tdodge.consulting>
2023-04-28 22:48:48 -0700

    Use the head of the list since melpazoid already checks the directory

--- nix/lib/package-lint.nix
+++ nix/lib/package-lint.nix
@@ -6,19 +6,20 @@ let
     el-mock
     package-lint
   ]);
-  package_lint_script = (pkgs.lib.strings.concatMapStringsSep "
-" (target: ''
-(package-install-file "${target.name}")
+  package_lint_target = pkgs.lib.head targets;
+  package_lint_script = ''
+(package-install-file "${package_lint_target.name}")
 (require 'package-lint)
 (message "[NOT RESPONSIBLE FOR WARNINGS PRIOR TO THIS POINT]")
+(find-file "${package_lint_target.name}")
 (checkdoc-eval-current-buffer)
 (with-current-buffer "*Style Warnings*" (message "%s" (buffer-string)))
 (package-lint-current-buffer)
-(byte-compile-file "${target.name}")
+(byte-compile-file "${package_lint_target.name}")
 (require 'melpazoid)
 (melpazoid-check-experimentals)
 (with-current-buffer "*Package-Lint*" (message "%s" (buffer-string)))
-'') targets);
+'';
   run_package_lint = pkgs.writeText "package-lint.el" package_lint_script;
   emacs = import ./environment.nix {
     inherit name;
