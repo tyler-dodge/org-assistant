69b01c709674245ddb1dfacd787ecb76db4d83f2
Tyler Dodge <tyler@tdodge.consulting>
2023-05-03 19:06:05 -0700

    Add support for overriding parameters to the model
    Also, Increase min version of emacs to 28.1. Deferred.el was having
    issues with 27.1 and 27.2
    
    Add end to end test for verification.

--- test/org-assistant-test.el
+++ test/org-assistant-test.el
@@ -3,15 +3,11 @@
 (require 'cl-lib)
 (require 'org)
 (require 'el-mock)
-
-(when (require 'undercover nil t)
-  (undercover "*.el"))
-(require 'org-assistant (expand-file-name "org-assistant.el"))
+(require 'ert-async)
 
 (ert-deftest org-assistant-exists ()
   "Sanity check to make sure expected symbols are exported."
   (should (functionp 'org-assistant)))
-
 
 (ert-deftest org-assistant-org-blocks-works-as-expected ()
   "Sanity check to make sure `org-assistant--org-blocks' works as expected."
@@ -30,43 +26,47 @@ System Prompt
 A
 #+END_SRC
 
-#+BEGIN_EXAMPLE
+#+BEGIN_SRC assistant :sender assistant
 A-response
-#+END_EXAMPLE
+#+END_SRC
 
 #+BEGIN_SRC assistant
 B
 #+END_SRC
 
-#+BEGIN_EXAMPLE
+#+BEGIN_SRC assistant :sender assistant
 B-response
-#+END_EXAMPLE
+#+END_SRC
 
 #+BEGIN_SRC ?
 C
 #+END_SRC
 
-#+BEGIN_EXAMPLE
+#+BEGIN_SRC assistant :sender assistant
 C-response
-#+END_EXAMPLE
+#+END_SRC
 ")
     (goto-char (point-min))
     (should (eq (org-assistant--org-blocks nil) nil))
     (search-forward "#+BEGIN_EXAMPLE")
     (goto-char (match-beginning 0))
-    (should (equal (org-assistant--org-blocks nil) (list (cons 'system "System Prompt"))))
+    (save-match-data (should (equal (org-assistant--org-blocks nil) (list (cons 'system "System Prompt")))))
     (search-forward "#+BEGIN_SRC")
     (goto-char (match-beginning 0))
-    (should (equal (org-assistant--org-blocks nil) (list
-                                                (cons 'system "System Prompt")
-                                                (cons 'user "A"))))
-    (search-forward "#+BEGIN_EXAMPLE")
+    (save-match-data
+      (should (equal (org-assistant--org-blocks nil) (list
+                                                      (cons 'system "System Prompt")
+                                                      (cons 'user "A")))))
+    (goto-char (match-end 0))
+    (search-forward "#+BEGIN_SRC assistant")
     (goto-char (match-beginning 0))
-    (should (equal (org-assistant--org-blocks nil)
-                   (list
-                    (cons 'system "System Prompt")
-                    (cons 'user "A")
-                    (cons 'assistant "A-response"))))
+    (save-match-data
+      (should (equal (org-assistant--org-blocks nil)
+                     (list
+                      (cons 'system "System Prompt")
+                      (cons 'user "A")
+                      (cons 'assistant "A-response")))))
+    (goto-char (match-end 0))
     (search-forward "#+BEGIN_SRC")
     (goto-char (match-beginning 0))
     (should (equal (org-assistant--org-blocks nil)
@@ -89,7 +89,7 @@ C-response
 
 (ert-deftest org-assistant-org-blocks-handles-multiple-branches ()
   "Sanity check to make sure `org-assistant--org-blocks' handles branches correctly."
-  (with-current-buffer (get-buffer-create " *assistant-test*")
+  (with-current-buffer (get-buffer-create "*assistant-test*")
     (erase-buffer)
     (org-mode)
     (insert "* Question
@@ -104,9 +104,9 @@ A
 A
 #+END_SRC
 
-#+BEGIN_EXAMPLE
+#+BEGIN_SRC assistant :sender assistant
 A-response
-#+END_EXAMPLE
+#+END_SRC
 ** B Branch
 #+BEGIN_SRC assistant
 B
@@ -116,9 +116,9 @@ B
 Ignore me B
 #+END_SRC
 
-#+BEGIN_EXAMPLE
+#+BEGIN_SRC assistant :sender assistant
 B-response
-#+END_EXAMPLE
+#+END_SRC
 
 ** C Branch
 #+BEGIN_SRC assistant
@@ -129,9 +129,9 @@ C
 Ignore me
 #+END_SRC
 
-#+BEGIN_EXAMPLE
+#+BEGIN_SRC assistant :sender assistant
 C-response
-#+END_EXAMPLE
+#+END_SRC
 ")
     (goto-char (point-min))
     (search-forward "B-response")
@@ -174,9 +174,9 @@ A <<substitution-A>>
 A
 #+END_SRC
 
-#+BEGIN_EXAMPLE
+#+BEGIN_SRC assistant :sender assistant
 A-response
-#+END_EXAMPLE
+#+END_SRC
 ** B Branch
 #+BEGIN_SRC assistant
 B
@@ -186,9 +186,9 @@ B
 Ignore me B
 #+END_SRC
 
-#+BEGIN_EXAMPLE
+#+BEGIN_SRC assistant :sender assistant
 B-response
-#+END_EXAMPLE
+#+END_SRC
 
 ** C Branch
 #+BEGIN_SRC assistant
@@ -199,9 +199,9 @@ C
 Ignore me
 #+END_SRC
 
-#+BEGIN_EXAMPLE
+#+BEGIN_SRC assistant :sender assistant
 C-response
-#+END_EXAMPLE
+#+END_SRC
 ")
     (goto-char (point-min))
     (search-forward "<<substitution-A>>")
@@ -224,5 +224,116 @@ C-response
                      (assistant . "A-response")
                      (user . "C")
                      (assistant . "C-response"))))))
+
+(ert-deftest-async org-assistant-end-to-end-test (done done-output-replaced)
+  "Sanity check to make sure `org-assistant--org-blocks' handles branches correctly."
+  (setq org-assistant-chat-extra-parameters-alist '((temperature . 2)
+                                                    (stop . "abc")))
+  (with-current-buffer (get-buffer-create " *assistant-test*")
+    (setq org-assistant-execute-curl-process-function
+          (lambda (&rest args)
+            (funcall done)
+            (-let* (((&keys :url :body :method :buffer) args) 
+                    (file (make-temp-file "org-json" nil nil
+                                          (org-assistant--json-encode '(("choices" . [(("message" ("content" . "TEST")))]))))))
+              (should (string= url (org-assistant-chat-endpoint)))
+              (should (string= method "POST"))
+              (let ((json (org-assistant--json-decode
+                           (with-current-buffer (find-file-noselect (substring (cadr (plist-get args :body)) 1))
+                             (buffer-string)))))
+                (should (equal json
+                               '((messages . [((content . "System Prompt") (role . "system"))
+                                              ((content . "A <<substitution-A>>") (role . "user"))
+                                              ((content . "A-response") (role . "assistant"))
+                                              ((content . "C") (role . "user"))
+                                              ((content . "C-response") (role . "assistant"))])
+                                 (stop . "abc")
+                                 (temperature . 1)
+                                 (model . "gpt-3.5-turbo"))))
+                (should (org-assistant--json-encode json))
+                (make-process
+                 :name "org-assistant-json-echo"
+                 :buffer buffer
+                 :filter #'internal-default-process-filter
+                 :command (list "cat" file))))))
+    (org-mode)
+    (erase-buffer)
+    (insert "* Question
+#+BEGIN_EXAMPLE
+System Prompt
+#+END_EXAMPLE
+
+#+NAME: substitution-A
+#+BEGIN_SRC text
+SUBSTITUTION
+#+END_SRC
+
+#+BEGIN_SRC assistant :noweb yes
+A <<substitution-A>>
+#+END_SRC
+
+#+BEGIN_SRC ignored
+A
+#+END_SRC
+
+#+BEGIN_SRC assistant :sender assistant
+A-response
+#+END_SRC
+** B Branch
+#+BEGIN_SRC assistant
+B
+#+END_SRC
+
+#+BEGIN_SRC text
+Ignore me B
+#+END_SRC
+
+#+BEGIN_SRC assistant :sender assistant
+B-response
+#+END_SRC
+
+** C Branch
+#+BEGIN_SRC assistant
+C
+#+END_SRC
+
+#+BEGIN_SRC text
+Ignore me
+#+END_SRC
+
+#+BEGIN_SRC assistant :sender assistant :params '((temperature . 1))
+C-response
+#+END_SRC
+")
+    (goto-char (point-min))
+    (search-forward "c-response")
+    (should (equal (org-assistant--org-blocks t)
+                   '((system . "System Prompt")
+                     (user . "A SUBSTITUTION")
+                     (assistant . "A-response")
+                     (user . "C")
+                     (assistant . "C-response"))))
+    (defun yes-or-no-p (prompt) t)
+    (let* ((uuid (org-ctrl-c-ctrl-c))
+           (location (save-excursion (goto-char (point-min))
+                                     (re-search-forward (rx (literal uuid)))
+                                     (match-beginning 0)))
+           (timer-cons (cons nil nil)))
+      (should (stringp uuid))
+      (goto-char location)
+      (should (looking-at-p (rx (literal uuid))))
+      (let ((buffer (current-buffer)))
+        (setcar timer-cons
+                (run-at-time
+                 nil 0.1
+                 (lambda ()
+                   (with-current-buffer buffer
+                     (goto-char location)
+                     (when (not (looking-at-p (rx (literal uuid))))
+                       (should (looking-at-p (rx line-start "#+BEGIN_SRC assistant :sender assistant"))) (forward-line 1)
+                       (should (looking-at-p (rx line-start "TEST" line-end))) (forward-line 1)
+                       (should (looking-at-p (rx line-start "#+END_SRC")))
+                       (cancel-timer (car timer-cons))
+                       (funcall done-output-replaced))))))))))
 
 (provide 'org-assistant-test)
